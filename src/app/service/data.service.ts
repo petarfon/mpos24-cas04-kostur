907882fa7cd83d8b052c3259817a005a2cc87b8e
Milica Simic <milica.simic1999@gmail.com>
2023-10-24 20:20:31 +0200

    servis za crud operacije

---
+++ src/app/service/data.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+
+import {
+  collection,
+  Firestore,
+  collectionData,
+  doc,
+  deleteDoc, 
+  addDoc
+ } from '@angular/fire/firestore';
+
+//  atributi inferfejsa se moraju poklapati sa poljima u dokumentu Firebase baze podataka
+ export interface Task {
+  id?: number;
+  date: string;
+  done: boolean;
+  name: string;
+  category: string;
+ }
+ 
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class DataService {
+
+  constructor(private firestore: Firestore ) { }
+
+  getTasks() {
+    const tasksRef = collection(this.firestore, 'tasks');
+    return collectionData(tasksRef, { idField: 'id' });
+  }
+
+  //sa stringom
+  // deleteTask(id: string) {
+  //   const taskRef = doc(this.firestore, `tasks/${id}`);
+  //   return deleteDoc(taskRef);
+  // }
+
+  //sa celim objektom
+  deleteTask(task: Task) {
+    const taskRef = doc(this.firestore, `tasks/${task.id}`);
+    return deleteDoc(taskRef);
+  }
+   
+}
